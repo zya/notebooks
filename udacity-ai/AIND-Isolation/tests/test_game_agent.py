d8ef97db622c082d00c52ce336eae29f07021069
Ehsan Ziya <ehsan.ziya@gmail.com>
2018-03-16 10:42:33 +0000

    Task: Bring the AIND-Isolation projecT

---
+++ udacity-ai/AIND-Isolation/tests/test_game_agent.py
@@ -0,0 +1,29 @@
+"""This file is provided as a starting template for writing your own unit
+tests to run and debug your minimax and alphabeta agents locally.  The test
+cases used by the project assistant are not public.
+"""
+
+import unittest
+
+import isolation
+import game_agent
+
+from importlib import reload
+
+
+class IsolationTest(unittest.TestCase):
+    """Unit tests for isolation agents"""
+
+    def setUp(self):
+        reload(game_agent)
+        self.player1 = "Player1"
+        self.player2 = "Player2"
+        self.game = isolation.Board(self.player1, self.player2)
+        self.minimax_player = game_agent.MinimaxPlayer()
+
+    def test_minimax(self):
+        self.minimax_player.minimax(self.game, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
